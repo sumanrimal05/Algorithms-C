c5215072a8c4d510c5a3a6401a68e7c4c64c315c
sumanrimal05 <sumanrimalconnect@gmail.com>
2025-01-09 17:30:25 +0545

    Recursive Insert implemented

--- Trees/binarySearchTree.c
+++ Trees/binarySearchTree.c
@@ -16,7 +16,9 @@ struct Node
 
 // Function Prototype
 struct Tree *bst_create();
-void bst_insert(struct Tree *tree, int value);
+struct Node *bst_node_create(int value);
+void bst_insert(struct Node **root, int value);
+void bst_insert_iteratively(struct Tree *tree, int value);
 void bst_delete(struct Tree *tree, int value);
 void bst_search(struct Tree *tree, int value);
 void bst_traverse_inOrder(struct Tree *tree);   // Left, Root, Right
@@ -31,32 +33,83 @@ int main()
 {
   // int input[] = {15, 10, 20, 8, 12, 17, 25};
   struct Tree *tree = bst_create();
-  bst_insert(tree, 15);
-  bst_insert(tree, 10);
-  bst_insert(tree, 20);
-  bst_insert(tree, 8);
-  bst_insert(tree, 12);
-  bst_insert(tree, 17);
-  bst_insert(tree, 25);
+  struct Node *root = tree->root = NULL;
+  bst_insert(&root, 10);
+  bst_insert(&root, 15);
+  bst_insert(&root, 20);
+  bst_insert(&root, 8);
+  bst_insert(&root, 12);
+  bst_insert(&root, 17);
+  bst_insert(&root, 25);
   return 0;
 }
 
 struct Tree *bst_create()
 {
   // Calloc allocates memory and assigns int to zero and pointer to NULL.
-  struct Tree *newTree = calloc(1, sizeof(struct Tree));
+  struct Tree *newTree = malloc(sizeof(struct Tree));
+  if (!newTree)
+  {
+    printf("Tree Create Fail: Memory allocation error.\n");
+    exit(1);
+  }
   return newTree;
 }
 
-void bst_insert(struct Tree *tree, int value)
+struct Node *bst_node_create(int value)
 {
-  struct Node *newNode = calloc(1, sizeof(struct Node));
+  struct Node *newNode = malloc(sizeof(struct Node));
+
+  if (!newNode)
+  {
+    printf("Node Create Error: Can't create node.\n");
+    exit(1);
+  }
+
+  newNode->left = NULL;
+  newNode->right = NULL;
   newNode->value = value;
 
+  return newNode;
+}
+
+// Use recursion to insert into BST
+void bst_insert(struct Node **root, int value)
+{
+
+  // if root is null
+  if (*root == NULL)
+  {
+    *root = bst_node_create(value);
+    return;
+  }
+
+  // Insert to left
+  else if (value <= (*root)->value)
+  {
+    bst_insert(&((*root)->left), value);
+  }
+  // insert to right
+  else
+  {
+    bst_insert(&((*root)->right), value);
+  }
+}
+
+// Insert into BST iteratively and doesnot allow duplicate
+void bst_insert_iteratively(struct Tree *tree, int value)
+{
+  struct Node *newNode = calloc(1, sizeof(struct Node));
+
+  if (!newNode)
+  {
+    printf("Node Create Fail: Memory allocation error.\n");
+    exit(1);
+  }
+  newNode->value = value;
   // Check if tree is empty
   if (!tree->root)
   {
-
     tree->root = newNode;
     return;
   }
@@ -64,6 +117,7 @@ void bst_insert(struct Tree *tree, int value)
   struct Node *temp = tree->root;
   while (temp)
   {
+
     // Left Insertion
     if (value < temp->value)
     {
@@ -87,7 +141,6 @@ void bst_insert(struct Tree *tree, int value)
     }
     else
     {
-      printf("%d\n", temp->value);
       // Duplicate value
       printf("Insert Error: Duplicate Value %d.\n", value);
       return;
